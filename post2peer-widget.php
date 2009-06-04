86241b1f3f7154f793712da783cc1f8f3b1c7c9a
ydubois <ydubois@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-04 13:54:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/post2peer-widget/branches/0.2@122881 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post2peer-widget.php
+++ post2peer-widget.php
@@ -2,7 +2,7 @@
 /**
  * @package YD_P2P-Widget
  * @author Yann Dubois
- * @version 0.1.0
+ * @version 0.2.0
  */
 
 /*
@@ -10,7 +10,7 @@
  Plugin URI: http://www.yann.com/wp-plugins/post2peer-wordpress-plugin
  Description: Allows to exchange and share post links + thumbnails across Wordpress blogs, based on matching tags and categories.
  Author: Yann Dubois
- Version: 0.1.0
+ Version: 0.2.0
  Author URI: http://www.yann.com/
  */
 
@@ -34,10 +34,18 @@
 /**
  Revision 0.1.0:
  - Initial release
- */
+ Revision 0.2.0:
+ - default options at install
+ - Bottom credits optional (additional checkbox) / no more link+text option
+ - Porn filter options (pornlover=1, justporn=1) 
+ - TODO:fixed image style (was not implemented)
+  */
 /**
  *	TODO:
- *  - Test, debug, final release
+ *  - Code and css mutualization with yd_recent_posts_widget
+ *  - Allow multiple widgets
+ *  - Choose specific default tags
+ *  - Custom choice of agregated RSS with site: syntax
  */
 
 /** Create Text Domain For Translations **/
@@ -49,6 +57,50 @@ function yd_p2p_widget_textdomain() {
 		'wp-content/plugins/' . $plugin_dir, $plugin_dir 
 	);
 }
+
+/** Install or reset plugin defaults **/
+function yd_p2p_plugin_reset( $force ) {
+	/** Init values **/
+	$yd_p2p_plugin_version	= "0.2.0";
+	$default_image_width	= 60;
+	$default_image_height	= 60;
+	$default_image_style	= 'padding-right:5px;padding-bottom:5px;float:left;';
+	$default_date_format	= __('F j', 'yd-p2p-widget');
+	$default_link			= 'http://post2peer.com/about';
+	$default_text			= 'Powered by Post2Peer beta';
+	$default_thumbnail_img	= 'http://www.yann.com/yd-post2peer-widget-v020-logo.gif';
+	$newoption				= 'yd_p2p_widget';
+	$newvalue				= '';
+	if( $df = get_option( 'date_format' ) ) $default_date_format = $df;
+	/** TODO **/
+	//$default_image_width = get_option( 'thumbnail_size_w' ) ? get_option( 'thumbnail_size_w' ) : $default_image_width;
+	//$default_image_height = get_option( 'thumbnail_size_h' ) ? get_option( 'thumbnail_size_h' ) : $default_image_height;
+	// ...this would need to generate the CSS file dynamically at plugin init
+	$default_image_style =	'width:' . $default_image_width . 'px;' .
+							'height:' . $default_image_height . 'px;' . $default_image_style;
+	$prev_options = get_option( $newoption );
+	if( ( isset( $force ) && $force ) || !isset($prev_options['plugin_version']) ) {
+		// those default options are set-up at plugin first-install or manual reset only
+		// they will not be changed when the plugin is just upgraded or deactivated/reactivated
+		$newvalue['plugin_version'] = $yd_p2p_plugin_version;
+		$newvalue['style'] = $default_image_style;
+		$newvalue['date_format'] = $default_date_format;
+		$newvalue['link'] = $default_link;
+		$newvalue['text'] = $default_text;
+		$newvalue['home_bottomlink'] = $default_bottomlink;
+		$newvalue['home_bottomtext'] = $default_bottomtext;
+		$newvalue['default_image'] = $default_thumbnail_img;
+		$newvalue['load_css'] = 1;
+		$newvalue['credits'] = 1;
+		$newvalue['language'] = 'en';
+		if( $prev_options ) {
+			update_option( $newoption, $newvalue );
+		} else {
+			add_option( $newoption, $newvalue );
+		}
+	}
+}
+register_activation_hook(__FILE__, 'yd_p2p_plugin_reset');
 
 /** Widget function: P2P **/
 function yd_p2p_widget( $args ) {
@@ -67,11 +119,9 @@ function yd_p2p_widget( $args ) {
 		__( 'Post2Peer beta', 'yd-p2p-widget' ) : 
 		apply_filters( 'widget_title', $options['title'] );
 	$link = empty( $options['link'] ) ? 
-		__( 'http://post2peer.com/about', 'yd-p2p-widget' ) : 
-		apply_filters( 'widget_title', $options['link'] );
+		__( 'http://post2peer.com/about', 'yd-p2p-widget' ) : $options['link'];
 	$text = empty( $options['text'] ) ? 
-		__( 'Powered by Post2Peer beta', 'yd-p2p-widget' ) : 
-		apply_filters( 'widget_title', $options['text'] );
+		__( 'Powered by Post2Peer beta', 'yd-p2p-widget' ) : $options['text'];
 		$html .= $before_widget;
 	if( $options['load_css'] ) 		
 		$html .= '<link type="text/css" rel="stylesheet" href="' . 
@@ -81,7 +131,9 @@ function yd_p2p_widget( $args ) {
 	$rss_img = '<img src="/wp-includes/images/rss.png" style="float:right;margin: 0 5px 0 0;" alt="Post2Peer custom rss" />';
 	$rss_img = '<a href="' . $rss_url . '" title="Post2Peer custom RSS feed" target="_out">' . $rss_img . '</a>';
 	if( $title ) $html .= $before_title . $rss_img . $title . $after_title . $listhtml;
-	$html .= '<small style="float:right"><a href="' . $link . '" target="_out">' . $text . '</a></small>';
+	if( $options['credits'] ) {
+		$html .= '<small style="float:right"><a href="' . $link . '" target="_out">' . $text . '</a></small>';
+	}
 	$html .= $after_widget;
 	// --
 		
@@ -106,11 +158,11 @@ function yd_p2p_widget_control() {
 	if ( isset($_POST['p2p_widget-submit']) ) {
 		$newoptions['title']	= strip_tags(stripslashes($_POST['p2p_widget-title']));
 		$newoptions['language']	= strip_tags(stripslashes($_POST['p2p_widget-language']));
-		$newoptions['link']		= strip_tags(stripslashes($_POST['p2p_widget-link']));
-		$newoptions['text']		= strip_tags(stripslashes($_POST['p2p_widget-text']));
+		$newoptions['credits']	= strip_tags(stripslashes($_POST['p2p_widget-credits']));
 		$newoptions['debug']	= strip_tags(stripslashes($_POST['p2p_widget-debug']));
 		$newoptions['min']		= strip_tags(stripslashes($_POST['p2p_widget-min']));
 		$newoptions['max']		= strip_tags(stripslashes($_POST['p2p_widget-max']));
+		$newoptions['porn']		= strip_tags(stripslashes($_POST['p2p_widget-porn']));
 		$newoptions['load_css']	= strip_tags(stripslashes($_POST['p2p_widget-load_css']));
 		$newoptions['style']	= strip_tags(stripslashes($_POST['p2p_widget-style']));
 		if( !$newoptions['load_css'] && !$newoptions['style'] ) $newoptions['style'] = $default_image_style;
@@ -123,15 +175,15 @@ function yd_p2p_widget_control() {
 
 	$title		= attribute_escape( $options['title'] );
 	$language	= attribute_escape( $options['language'] );
-	$text 		= attribute_escape( $options['text'] );
-	$link 		= attribute_escape( $options['link'] );
+	$credits 	= attribute_escape( $options['credits'] );
 	$debug 		= attribute_escape( $options['debug'] );
 	$min 		= attribute_escape( $options['min'] );
 	$max 		= attribute_escape( $options['max'] );
+	$porn 		= attribute_escape( $options['porn'] );
 	$load_css 	= attribute_escape( $options['load_css'] );
 	$style 		= attribute_escape( $options['style'] );
 	?>
-	<img src="http://www.yann.com/yd-post2peer-widget-v010-logo.gif" style="float:right;width:10px;height:10px;">
+	<img src="http://www.yann.com/yd-post2peer-widget-v020-logo.gif" style="float:right;width:10px;height:10px;">
 	<p><label for="p2p_widget-title">
 	<?php _e('Title:') ?> 
 	<input type="text" class="widefat" id="p2p_widget-title" name="p2p_widget-title" 
@@ -150,16 +202,6 @@ function yd_p2p_widget_control() {
 	?>
 	</select></label></p>
 	
-	<p><label for="p2p_widget-link">
-	<?php _e('Link:') ?> 
-	<input type="text" class="widefat" id="p2p_widget-link" name="p2p_widget-link" 
-		value="<?php echo $link ?>" /></label></p>
-		
-	<p><label for="p2p_widget-text">
-	<?php _e('Text:') ?> 
-	<input type="text" class="widefat" id="p2p_widget-text" name="p2p_widget-text" 
-		value="<?php echo $text ?>" /></label></p>
-		
 	<p><label for="p2p_widget-min">
 	<?php _e('Minimum number of links:') ?> 
 	<input type="text" class="widefat" id="p2p_widget-min" name="p2p_widget-min" 
@@ -170,6 +212,11 @@ function yd_p2p_widget_control() {
 	<input type="text" class="widefat" id="p2p_widget-max" name="p2p_widget-max" 
 		value="<?php echo $max ?>" /></label></p>
 		
+	<p><label for="p2p_widget-porn">
+	<?php _e('Allow porn:') ?> 
+	<input type="checkbox" class="widefat" id="p2p_widget-porn" name="p2p_widget-porn" 
+		value="1" <?php if( $porn ) echo ' checked="checked" ' ?> /></label></p>
+			
 	<p><label for="p2p_widget-load_css">
 	<?php _e('Load stylesheet:') ?> 
 	<input type="checkbox" class="widefat" id="p2p_widget-load_css" name="p2p_widget-load_css" 
@@ -180,6 +227,11 @@ function yd_p2p_widget_control() {
 	<input type="text" class="widefat" id="p2p_widget-style" name="p2p_widget-style" 
 		value="<?php echo $style ?>" /></label></p>
 
+	<p><label for="p2p_widget-credits">
+	<?php _e('Display credits:') ?> 
+	<input type="checkbox" class="widefat" id="p2p_widget-credits" name="p2p_widget-credits" 
+		value="1" <?php if( $credits ) echo ' checked="checked" ' ?> /></label></p>
+			
 	<p><label for="p2p_widget-debug">
 	<?php _e('Debug:') ?> 
 	<input type="checkbox" class="widefat" id="p2p_widget-debug" name="p2p_widget-debug" 
@@ -232,6 +284,7 @@ function yd_p2p_widget_main() {
 		'&e=' . get_bloginfo( 'home' ) .
 		'&max=' . $options['max'] .
 		'&min=' . $options['min'];
+	if( $options['porn'] ) $url .= '&pornlover=1';
 	$rss = yd_fetch_rss( $url );
 	if( $h = yd_p2p_walk_rss( $rss ) ) $html .= $h;
 	if( $options['debug'] ) {
@@ -269,7 +322,9 @@ function yd_p2p_walk_rss( $rss ) {
 			$desc = yd_clean_cut( $desc, 128 );
 			if( preg_match( "/<img[^>]+>/", $item['content']['encoded'], $matches ) ) $img = $matches[0];
 			if( preg_match( "|<a[^>]+href=\"([^\"]+)\"[^>]*>[^<]+</a></p>$|", $item['content']['encoded'], $matches ) ) $link = $matches[1];
-			
+			if( !$options['load_css'] && $options['style'] ) 
+				$img = preg_replace( "|style=\"[^\"]+\"|", ' style="' . $options['style'] . '"', $img );
+				
 			$html .= '<li><h4><a href="' . $link . '" rel="bookmark" title="Permanent link to: ' . $title . '">' . $img . $title . '</a></h4>';
 			$html .= '<div class="yd_rp_excerpt">' . $desc;
 			$html .= $direct_link;
@@ -286,11 +341,11 @@ function yd_p2p_walk_rss( $rss ) {
 
 function yd_get_p2p_language_list() {
 	$lang = Array(
+		'en' => 'English',
 		'ar' => 'العربية',
 		'cs' => 'Česky',
 		'da' => 'Dansk',
 		'de' => 'Deutsch',
-		'en' => 'English',
 		'es' => 'Español',
 		'fr' => 'Français',
 		'he' => 'עברית',
